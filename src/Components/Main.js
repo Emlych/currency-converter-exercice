fd6d7875c45d51e0e69db48c5a4df8b0b82728ea
Emily LD <lagreouemily@gmail.com>
2022-02-03 18:26:47 +0100

    functionnal converter two ways

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,18 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 const Main = () => {
-  const [money, setmoney] = useState(0);
-  const handleMoney = (event) => {
-    setmoney(event.target.value);
+  const [eur, setEur] = useState(0);
+  const [usd, setUsd] = useState(0);
+
+  const convertEur = (event) => {
+    setEur(event.target.value);
+    setUsd(event.target.value * 1.14);
+  };
+  const convertUsd = (event) => {
+    setUsd(event.target.value);
+    setEur(event.target.value * 0.87);
   };
   return (
     <div className="main">
       <div className="line">
-        <input type="text" value={money} onChange={handleMoney} />€
+        <input type="text" value={eur} onChange={convertEur} />€
       </div>
-      <div className="symbol">🔽</div>
+      <div className="symbol">🔽 🔼</div>
       <div className="line">
-        <input type="text" value={money * 1.14} /> $
+        <input type="text" value={usd} onChange={convertUsd} /> $
       </div>
     </div>
   );
