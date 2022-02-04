d4324d265c0f56e0413e69fc464b265e6f7a9ad2
Emily LD <lagreouemily@gmail.com>
2022-02-04 07:46:34 +0100

    functionnal conversion two ways

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import rates from "../assets/currency";
+import Line from "./Line";
 
 const Main = () => {
-  //   const [eur, setEur] = useState(0);
-  //   const [usd, setUsd] = useState(0);
-
-  //   const convertEur = (event) => {
-  //     setEur(event.target.value);
-  //     setUsd(event.target.value * 1.14);
-  //   };
-  //   const convertUsd = (event) => {
-  //     setUsd(event.target.value);
-  //     setEur(event.target.value * 0.87);
-  //   };
-
   const [cur1, setCur1] = useState(0);
   const [cur2, setCur2] = useState(0);
   const [curType1, setCurType1] = useState("EUR");
@@ -23,23 +12,19 @@ const Main = () => {
   const currency = Object.keys(rates);
 
   const handleCurChange1 = (event) => {
-    console.log("currency ===>", event.target.value);
     setCurType1(event.target.value);
-    console.log("taux de conversion ===>", rates[event.target.value]);
   };
   const handleCurChange2 = (event) => {
-    console.log("currency ===>", event.target.value);
     setCurType2(event.target.value);
-    console.log("taux de conversion ===>", rates[event.target.value]);
   };
 
   const convertCur1 = (event) => {
     setCur1(event.target.value);
-    setCur2(event.target.value * rates[curType1]);
+    setCur2((event.target.value * rates[curType2]) / rates[curType1]);
   };
   const convertCur2 = (event) => {
     setCur2(event.target.value);
-    setCur1(event.target.value * rates[curType2]);
+    setCur1((event.target.value * rates[curType1]) / rates[curType2]);
   };
 
   return (
